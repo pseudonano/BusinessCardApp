4e26f6e253d7076b43f0356c00dc1461e0bc0b9d
teguhpambudif <teguhpambudi1251@gmail.com>
2023-10-14 14:35:51 +0700

    extract string resource

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,7 +60,7 @@ fun BusinessApp(){
                 .fillMaxWidth(),
             contentAlignment = Alignment.Center
         ) {
-            MainInfo(name = "Teguh Pambudi", title = "Software Engineer wannabe")
+            MainInfo(name = stringResource(R.string.name), title = stringResource(R.string.title))
             Box(
                 modifier = Modifier
                     .fillMaxSize()
@@ -68,9 +69,9 @@ fun BusinessApp(){
             ) {
                 Column {
                     InfoCard(
-                        phone = "+085123456789",
-                        email = "teguhteguh@mail.com",
-                        social = "@teguhteguh"
+                        phone = stringResource(R.string.phone),
+                        email = stringResource(R.string.mail),
+                        social = stringResource(R.string.social)
                     )
                 }
             }
